451c11a985193f323d1d3721cf4a13acdda0ebcb
TranDuy <tran.duy@mulodo.com>
2015-01-14 16:50:31 +0700

    updated database design

--- doc/backend/db/TRN-MiniBlog_DatabaseInstall_TranAnhDuy.sql
+++ doc/backend/db/TRN-MiniBlog_DatabaseInstall_TranAnhDuy.sql
@@ -18,17 +18,21 @@ USE `miniblog` ;
 -- Table `miniblog`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `miniblog`.`users` (
-  `id` INT(11) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(20) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `firstname` VARCHAR(45) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
-  `avatar` VARCHAR(99) NOT NULL,
+  `avatar` VARCHAR(100) NOT NULL,
   `gender` TINYINT(1) NOT NULL DEFAULT 0,
   `birthdate` DATETIME NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
   `boss` TINYINT(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`),
+  INDEX `idx_username` (`username` ASC),
+  INDEX `idx_firstname` (`firstname` ASC),
+  INDEX `idx_lastname` (`lastname` ASC),
+  INDEX `idx_fullname` (`firstname` ASC, `lastname` ASC))
 ENGINE = InnoDB;
 
 
@@ -36,15 +40,17 @@ ENGINE = InnoDB;
 -- Table `miniblog`.`posts`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `miniblog`.`posts` (
-  `id` INT(11) NOT NULL,
-  `title` VARCHAR(45) NOT NULL,
-  `description` MEDIUMTEXT NOT NULL,
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `description` LONGTEXT NOT NULL,
   `created_at` DATETIME NULL,
   `updated_at` DATETIME NULL,
   `deactived` TINYINT(1) NOT NULL DEFAULT 0,
-  `users_id` INT(11) NOT NULL,
+  `users_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`, `users_id`),
   INDEX `fk_posts_users1_idx` (`users_id` ASC),
+  INDEX `inx_desc_posts` (`description`(1) ASC),
+  INDEX `inx_deact_posts` (`deactived` ASC),
   CONSTRAINT `fk_posts_users1`
     FOREIGN KEY (`users_id`)
     REFERENCES `miniblog`.`users` (`id`)
@@ -57,17 +63,18 @@ ENGINE = InnoDB;
 -- Table `miniblog`.`comments`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `miniblog`.`comments` (
-  `id` INT(11) NOT NULL,
-  `title` VARCHAR(99) NOT NULL,
-  `description` MEDIUMTEXT NOT NULL,
+  `id` INT(11) UNSIGNED NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `description` LONGTEXT NOT NULL,
   `created_at` DATETIME NULL,
   `updated_at` DATETIME NULL,
   `deactived` TINYINT(1) NOT NULL DEFAULT 0,
-  `posts_id` INT(11) NOT NULL,
-  `users_id` INT(11) NOT NULL,
+  `posts_id` INT(11) UNSIGNED NOT NULL,
+  `users_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`, `posts_id`, `users_id`),
   INDEX `fk_comments_posts_idx` (`posts_id` ASC),
   INDEX `fk_comments_users1_idx` (`users_id` ASC),
+  INDEX `idx_desc_comments` (`description`(1) ASC),
   CONSTRAINT `fk_comments_posts`
     FOREIGN KEY (`posts_id`)
     REFERENCES `miniblog`.`posts` (`id`)
